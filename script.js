2d3a64ea3f5abbee471808200e4bf669965df4ba
razz1000 <76906581+razz1000@users.noreply.github.com>
2022-04-06 18:24:36 +0100

    Buttons are now working

--- script.js
+++ script.js
@@ -1,63 +1,119 @@
+    /* This is the load images function. Everything is inside it  */
+    let loadImagesFunction = (event) => {
+        console.log(event.target)
 
         fetch("https://striveschool-api.herokuapp.com/api/deezer/search?q=behemoth",) 
         .then(response => response.json())
         .then(nature => { 
             console.log(nature.data[0].album.cover_medium)
-         
-           
-              let cards2 = document.getElementById("svg1");
-            cards2.remove();
-            let div4 = document.getElementById("div4")
+  
+            /* I start by removing the whole row */
+            let row1 = document.getElementById("row1")
+            row1.remove();
 
-            let cards3 = document.createElement("div");
-            cards3.innerHTML = `<img src="${nature.data[0].album.cover_medium}" alt="">`
-            div4.appendChild(cards3)
+            /* Then I put in the new cards */
+            let container1 = document.getElementById("container1");
+            let row2 = document.createElement("div") 
+            row2.classList.add("row")
+            container1.appendChild(row2)
+            for (let i = 0; i < nature.data.length; i++) {   
+                let newCol = document.createElement("div")
+                newCol.classList.add("col-sm-3") 
+                newCol.innerHTML = `
+                <img src="${nature.data[i].album.cover_big}" class="card-img-top" alt="...">
+                  <div class="card-body">
+                    <p class="card-text">
+                      This is a wider card with supporting text below as a natural
+                      lead-in to additional content. This content is a little bit
+                      longer.
+                    </p>
+                    <div
+                      class="d-flex justify-content-between align-items-center"
+                    >
+                      <div class="btn-group">
+                        <button
+                          type="button"
+                          class="btn btn-sm btn-outline-secondary"
+                        >
+                          View
+                        </button>
+                        <button
+                          type="button"
+                          class="btn btn-sm btn-outline-secondary"
+                        >
+                          Edit
+                        </button>
+                      </div>
+                      <small class="text-muted">9 mins</small>
+                    </div>
+                  </div>             
+                `
+                row2.appendChild(newCol)
+            }
 
-            /* 
-            cards2.innerHTML = `<svg id="svg1" class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="${nature.data[0].album.cover_medium}" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Thumbnail"></svg>`
-          */
-        
-        
+
         })
-         
         .catch(err => console.error(err));
+            }
 
-
- 
-
-
-    let loadImagesFunction = (event) => {
+    /* This is the function for button 2 */
+    let loadImagesFunction2 = (event) => {
         console.log(event.target)
 
+        fetch("https://striveschool-api.herokuapp.com/api/deezer/search?q=metallica",) 
+        .then(response => response.json())
+        .then(nature => { 
+            console.log(nature.data[0].album.cover_medium)
+  
+            /* I start by removing the whole row */
+            let row3 = document.querySelector(".row")
+            row3.remove();
 
-         
-
-            for(let i = 0; i < nature.photos.lenght; i++) {
-
-                newSvg = document.createElement("div")
-                newSvg.classList("new-div")
-                newsvg.innerHTML = `
-                <svg
-                class="bd-placeholder-img card-img-top"
-                width="100%"
-                height="225"
-                xmlns="${nature.photos[i].src.landscape}"
-                preserveAspectRatio="xMidYMid slice"
-                focusable="false"
-                role="img"
-                aria-label="Placeholder: Thumbnail"
-              >
-                <title>Placeholder</title>
-                <rect width="100%" height="100%" fill="#55595c" />
-                <text x="50%" y="50%" fill="#eceeef" dy=".3em">
-                  Thumbnail
-                </text>
-              </svg>
+            /* Then I put in the new cards */
+            let container1 = document.getElementById("container1");
+            let row2 = document.createElement("div") 
+            row2.classList.add("row")
+            container1.appendChild(row2)
+            for (let i = 0; i < nature.data.length; i++) {   
+                let newCol = document.createElement("div")
+                newCol.classList.add("col-sm-3") 
+                newCol.innerHTML = `
+                <img src="${nature.data[i].album.cover_big}" class="card-img-top" alt="...">
+                  <div class="card-body">
+                    <p class="card-text">
+                      This is a wider card with supporting text below as a natural
+                      lead-in to additional content. This content is a little bit
+                      longer.
+                    </p>
+                    <div
+                      class="d-flex justify-content-between align-items-center"
+                    >
+                      <div class="btn-group">
+                        <button
+                          type="button"
+                          class="btn btn-sm btn-outline-secondary"
+                        >
+                          View
+                        </button>
+                        <button
+                          type="button"
+                          class="btn btn-sm btn-outline-secondary"
+                        >
+                          Edit
+                        </button>
+                      </div>
+                      <small class="text-muted">9 mins</small>
+                    </div>
+                  </div>             
                 `
-                cards2.appendChild(newSvg)
-
+                row2.appendChild(newCol)
             }
+
+
+        })
+        .catch(err => console.error(err));
+    }
 
  
             
-    }                
+               
